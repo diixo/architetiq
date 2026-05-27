40fb1b2bf655271095e6634677015fd97d32a30b
diixo <anatolii.podoba@gmail.com>
2026-05-28 02:18:56 +0300

    main.html: initial commit

--- ui/app_main/views.py
+++ ui/app_main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 
 def main(request):
-    return render(request, "app_main/index.html", context={
+    return render(request, "app_main/main.html", context={
         "title": "ArchitetIQ",
         "description": "ArchitetIQ description"})
 
